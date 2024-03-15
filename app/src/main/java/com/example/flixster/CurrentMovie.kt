3266458bf9c701111fa152893d755b99ad864d18
Natasha <wong.natasha.m@gmail.com>
2024-03-15 16:37:58 -0700

    Added CurrentMovie.kt, which is the model for storing a single movie from the API and added fragment_current_movie.xml, which is the layout for a CurrentMovie

--- app/src/main/java/com/example/flixster/CurrentMovie.kt
+++ app/src/main/java/com/example/flixster/CurrentMovie.kt
@@ -1,4 +1,25 @@
 package com.example.flixster
 
+import com.google.gson.annotations.SerializedName
+
+/**
+ * The Model for storing a single movie from the Movie Database API
+ *
+ * SerializedName tags MUST match the JSON response for the
+ * object to correctly parse with the gson library.
+ */
 class CurrentMovie {
+
+    @JvmField
+    @SerializedName("title")
+    var title: String? = null
+
+
+    @SerializedName("movie_image")
+    var movieImageUrl: String? = null
+
+
+    @SerializedName("description")
+    var description: String? = null
+
 }
