5be77ae5d071601a75e3e950c5a821ff2a0ed1ff
Natasha <wong.natasha.m@gmail.com>
2024-03-15 19:17:42 -0700

    Added CurrentMoviesFragment.kt and internet permission, have an issue with crashing on startup

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.flixster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.flixster.R.id
 
+/**
+ * The MainActivity for the BestSellerList app.
+ * Launches a [CurrentMoviesFragment].
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val supportFragmentManager = supportFragmentManager
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(id.content, CurrentMoviesFragment(), null).commit()
     }
 }
