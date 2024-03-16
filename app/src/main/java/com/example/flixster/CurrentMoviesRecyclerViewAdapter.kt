5ed0f714617bb3f0183a1519717b9173e949587f
Natasha <wong.natasha.m@gmail.com>
2024-03-15 17:49:06 -0700

    Added CurrentMoviesRecyclerViewAdapter.kt which can display a CurrentMovie

---
+++ app/src/main/java/com/example/flixster/CurrentMoviesRecyclerViewAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.flixster
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.flixster.R.id
+
+/**
+ * [RecyclerView.Adapter] that can display a [CurrentMovie]
+ */
+class CurrentMoviesRecyclerViewAdapter (private val movies: List<CurrentMovie>)
+    : RecyclerView.Adapter<CurrentMoviesRecyclerViewAdapter.MovieViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_current_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    /**
+     * This inner class lets us refer to all the different View elements
+     * (Yes, the same ones as in the XML layout files!)
+     */
+    inner class MovieViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        var mItem: CurrentMovie? = null
+        val mMovieTitle: TextView = mView.findViewById<View>(id.movie_title) as TextView
+        val mMovieDescription: TextView = mView.findViewById<View>(id.movie_description) as TextView
+        val mMovieImage: ImageView = mView.findViewById<View>(id.movie_image) as ImageView
+
+        override fun toString(): String {
+            return mMovieTitle.toString()
+        }
+    }
+
+    /**
+     * This lets us "bind" each Views in the ViewHolder to its' actual data!
+     */
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = movies[position]
+
+        holder.mItem = movie
+        holder.mMovieTitle.text = movie.title
+        holder.mMovieDescription.text = movie.description
+
+        Glide.with(holder.mView)
+            .load(movie.movieImageUrl)
+            .centerInside()
+            .into(holder.mMovieImage)
+    }
+
+    /**
+     * Remember: RecyclerView adapters require a getItemCount() method.
+     */
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+}
