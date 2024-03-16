a4e6a8df65422229ea2eb4d5ffced8633ce3c216
Natasha <wong.natasha.m@gmail.com>
2024-03-15 22:43:39 -0700

    Fragments now load one per row, recyclerView.layoutManager spanCount set to 1 instead of 2

--- app/src/main/java/com/example/flixster/CurrentMoviesFragment.kt
+++ app/src/main/java/com/example/flixster/CurrentMoviesFragment.kt
@@ -16,7 +16,6 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import okhttp3.Headers
 import org.json.JSONArray
-import org.json.JSONObject
 
 
 // --------------------------------//
@@ -41,7 +40,7 @@ class CurrentMoviesFragment : Fragment() {
         val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
         val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
         val context = view.context
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        recyclerView.layoutManager = GridLayoutManager(context, 1)
         updateAdapter(progressBar, recyclerView)
         return view
     }
